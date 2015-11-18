c86198b55e35c63dd82d60be538c59c0d33f3763
ningsiping <ningsp@yimayholiday.com>
2015-11-18 16:55:46 +0800

    modify confict

---
+++ harem-web/src/main/java/com/yimayhd/harem/controller/loginout/TestController.java
@@ -0,0 +1,80 @@
+package com.yimayhd.harem.controller.loginout;
+
+import com.yimayhd.harem.controller.loginout.vo.LoginoutVO;
+import com.yimayhd.user.client.domain.UserDO;
+import com.yimayhd.user.client.enums.UserType;
+import com.yimayhd.user.client.service.UserService;
+import com.yimayhd.user.session.manager.SessionUtils;
+import net.pocrd.util.Md5Util;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by root on 15-11-9.
+ */
+@RestController
+public class TestController {
+
+    private UserService userService;
+
+    @RequestMapping("/test/test1")
+    public Map<String, Object> testOne() {
+        Map<String, Object> map = new HashMap<String, Object>();
+
+        HttpServletRequest httpServletRequest = SessionUtils.getRequest();
+        if (null == httpServletRequest) {
+            return map;
+        }
+
+        HttpSession httpSession = httpServletRequest.getSession();
+        httpSession.setAttribute("user","123");
+
+        map.put("status", "123");
+        return map;
+    }
+
+    @RequestMapping("/test/test2")
+    public Map<String, Object> testTwo() {
+        Map<String, Object> map = new HashMap<String, Object>();
+
+        HttpServletRequest httpServletRequest = SessionUtils.getRequest();
+        if (null == httpServletRequest) {
+            return map;
+        }
+
+        HttpSession httpSession = httpServletRequest.getSession();
+        Object test = httpSession.getAttribute("user");
+        if (null == test) {
+            map.put("status", "not login");
+            return map;
+        }
+
+        map.put("status", "has logined");
+        return map;
+    }
+
+
+    @RequestMapping("/extern/test/createUser")
+    public Map<String, Object> createUser(LoginoutVO loginoutVO) {
+        Map<String, Object> map = new HashMap<String, Object>();
+
+        UserDO userDO = new UserDO();
+        userDO.setMobile(loginoutVO.getUsername());
+        userDO.setUserType(UserType.U.getType());
+        userDO.setPassword(new String(Md5Util.compute((loginoutVO.getPassword() + "yimayhd.com%FG&G**HI%%F_q").getBytes())));
+        Date now = new Date();
+        userDO.setGmtCreate(now);
+        userDO.setGmtUpdate(now);
+        userService.createUser(userDO);
+
+
+        map.put("status", "create success");
+        return map;
+    }
+}
