1ab36d430b3dd7605ffe9af99922f53ed2499d2d
caozhangfeng <caozf@yimayholiday.com>
2015-11-24 13:46:06 +0800

    酒店商品

---
+++ harem-biz/src/main/java/com/yimayhd/harem/service/impl/CommodityServiceImpl.java
@@ -0,0 +1,16 @@
+package com.yimayhd.harem.service.impl;
+
+import com.yimayhd.harem.service.CommodityService;
+import com.yimayhd.ic.client.model.domain.item.ItemDO;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2015/11/24.
+ */
+public class CommodityServiceImpl implements CommodityService {
+    @Override
+    public List<ItemDO> getList() throws Exception {
+        return null;
+    }
+}
