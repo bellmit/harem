316a2868ce2604f0e78bbfacc68cbaaa748802d6
yebin <yebin@yimayholiday.com>
2015-11-27 17:56:23 +0800

    完成产品-线路页

--- harem-web/src/main/java/com/yimayhd/harem/controller/CommGroupTravelController.java
+++ harem-web/src/main/java/com/yimayhd/harem/controller/CommGroupTravelController.java
@@ -38,6 +38,18 @@ public class CommGroupTravelController extends BaseController {
 	private ScenicSpotService scenicSpotService;
 
 	/**
+	 * 详细信息页
+	 * 
+	 * @param id
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestMapping(value = "/detail", method = RequestMethod.GET)
+	public String detail() throws Exception {
+		return "/system/comm/groupTravel/detail";
+	}
+
+	/**
 	 * 基本信息页
 	 * 
 	 * @param id
@@ -46,7 +58,7 @@ public class CommGroupTravelController extends BaseController {
 	 */
 	@RequestMapping(value = "/baseInfo", method = RequestMethod.GET)
 	public String baseInfo() throws Exception {
-		return "/system/comm/groupTravel/baseInfo";
+		return "/system/comm/groupTral/baseInfo";
 	}
 
 	/**
