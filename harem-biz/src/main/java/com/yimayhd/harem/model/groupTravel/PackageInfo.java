95ee26728077af7331f4663dab0b9133cf14793f
yebin <yebin@yimayholiday.com>
2015-11-28 15:53:12 +0800

    为价格信息的套餐添加时间属性

--- harem-biz/src/main/java/com/yimayhd/harem/model/groupTravel/PackageInfo.java
+++ harem-biz/src/main/java/com/yimayhd/harem/model/groupTravel/PackageInfo.java
@@ -9,6 +9,8 @@ import java.util.List;
  *
  */
 public class PackageInfo {
+	private long startDate;
+	private long endDate;
 	private String name;// 套餐名
 	private String from;// 出发地
 	private List<PackageMonth> months;
@@ -35,6 +37,22 @@ public class PackageInfo {
 
 	public void setMonths(List<PackageMonth> months) {
 		this.months = months;
+	}
+
+	public long getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(long startDate) {
+		this.startDate = startDate;
+	}
+
+	public long getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(long endDate) {
+		this.endDate = endDate;
 	}
 
 }
