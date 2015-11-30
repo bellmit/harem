036eab8b1094a94007324d081bb1dcaef4657e4b
xuzejun <65416803@qq.com>
2015-11-30 10:17:21 +0800

    导入错误包，编译不通过，修正

--- harem-web/src/main/java/com/yimayhd/harem/base/JsonResultUtil.java
+++ harem-web/src/main/java/com/yimayhd/harem/base/JsonResultUtil.java
@@ -1,12 +1,11 @@
 package com.yimayhd.harem.base;
 
-import com.alibaba.fastjson.JSON;
-import com.sun.tools.internal.xjc.reader.xmlschema.bindinfo.BIConversion;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import javax.servlet.http.HttpServletResponse;
 import java.io.PrintWriter;
 import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+
+import com.alibaba.fastjson.JSON;
 
 /**
  * Created by Administrator on 2015/11/28.
@@ -21,6 +20,5 @@ public class JsonResultUtil {
         String jsonString = JSON.toJSONString(new ResponseVo(entity));
         out.println(jsonString);
         out.flush();
-        out.close();
-    }
+        out.close();    }
 }
