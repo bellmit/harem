243e737ab0ebf89496da90e641e322fa757118b2
yebin <yebin@yimayholiday.com>
2015-12-04 10:33:43 +0800

    调整每页记录数选择框

--- harem-web/src/main/webapp/WEB-INF/resources/js/page.js
+++ harem-web/src/main/webapp/WEB-INF/resources/js/page.js
@@ -20,7 +20,7 @@ var pageUtils = {
         root.append(pager);
         if (pageVo.hasPreviousPage) {
             var previous = $("<li class='previous'/>");
-            var LinkeToPage = $(" <a href='javascript:void(0)' class='LinkeToPage' pageNo='"
+            var LinkeToPage = $(" <a href='javascript:void(0);' class='LinkeToPage' pageNo='"
             + pageVo.previousPageNumber + "' id='prePage'>« 上一页</a>");
             LinkeToPage
                 .click(function () {
@@ -30,16 +30,16 @@ var pageUtils = {
             pager.append(previous.append(LinkeToPage));
         } else {
             var previous = $("<li class='previous disabled'/>");
-            var LinkeToPage = $("<a href='javascript:void(0)' id='prePage'>« 上一页</a>");
+            var LinkeToPage = $("<a href='javascript:void(0);' id='prePage'>« 上一页</a>");
             pager.append(previous.append(LinkeToPage));
         }
         for (var i = 1; i <= pageVo.lastPageNumber; i++) {
             if (pageVoNo == i) {
-                var li = $("<li class='active'<span><a href='javascript:void(0)'>"
+                var li = $("<li class='active'<span><a href='javascript:void(0);'>"
                 + i + "</a></span></li>");
                 pager.append(li);
             } else if (1 >= i - 1) {
-                var li = $("<li><span><a href='javascript:void(0)' class='LinkeToPage' pageNo='"
+                var li = $("<li><span><a href='javascript:void(0);' class='LinkeToPage' pageNo='"
                 + i + "'>" + i + "</a></span></li>");
                 li.find(".LinkeToPage").click(
                     function () {
@@ -48,7 +48,7 @@ var pageUtils = {
                     });
                 pager.append(li);
             } else if (pageVo.lastPageNumber <= i + 1) {
-                var li = $("<li><span><a href='javascript:void(0)' class='LinkeToPage' pageNo='"
+                var li = $("<li><span><a href='javascript:void(0);' class='LinkeToPage' pageNo='"
                 + i + "'>" + i + "</a></span></li>");
                 li.find(".LinkeToPage").click(
                     function () {
@@ -57,7 +57,7 @@ var pageUtils = {
                     });
                 pager.append(li);
             } else if (pageVoNo >= i - 2 && pageVoNo <= i + 2) {
-                var li = $("<li><span><a href='javascript:void(0)' class='LinkeToPage' pageNo='"
+                var li = $("<li><span><a href='javascript:void(0);' class='LinkeToPage' pageNo='"
                 + i + "'>" + i + "</a></span></li>");
                 li.find(".LinkeToPage").click(
                     function () {
@@ -66,18 +66,18 @@ var pageUtils = {
                     });
                 pager.append(li);
             } else if (pageVoNo > i && before == 0) {
-                var li = $("<li><a href='javascript:void(0)' data-toggle='pager' data-placement='top'>...</a></li>");
+                var li = $("<li><a href='javascript:void(0);' data-toggle='pager' data-placement='top'>...</a></li>");
                 pager.append(li);
                 before = 1;
             } else if (pageVoNo < i && after == 0) {
-                var li = $("<li><a href='javascript:void(0)' data-toggle='pager' data-placement='top'>...</a></li>");
+                var li = $("<li><a href='javascript:void(0);' data-toggle='pager' data-placement='top'>...</a></li>");
                 pager.append(li);
                 after = 1;
             }
         }
         if (pageVo.hasNextPage) {
             var next = $("<li class='next'/>");
-            var LinkeToPage = $(" <a href='javascript:void(0)' class='LinkeToPage' pageNo='"
+            var LinkeToPage = $(" <a href='javascript:void(0);' class='LinkeToPage' pageNo='"
             + pageVo.nextPageNumber + "' id='nextPage'>下一页»</a>");
             LinkeToPage
                 .click(function () {
@@ -87,13 +87,13 @@ var pageUtils = {
             pager.append(next.append(LinkeToPage));
         } else {
             var next = $("<li class='next disabled'/>");
-            var LinkeToPage = $("<a href='javascript:void(0)' id='nextPage'>下一页»</a>");
+            var LinkeToPage = $("<a href='javascript:void(0);' id='nextPage'>下一页»</a>");
             pager.append(next.append(LinkeToPage));
         }
-        var li = $("</li>");
-        var input_group = $("<div class='input-group' style='width: 189px;'/>");
+        var li = $("<li/>");
+        var input_group = $("<div class='input-group' style='width: 149px;'/>");
         input_group.append($("<span class='input-group-addon'>每页</span>"));
-        var select = $("<select class='form-control pageSize' name='pageSize' id='pageSize'/>");
+        var select = $("<select class='form-control pageSize' name='pageSize' id='pageSize' style='width: 60px;'/>");
         select.append($("<option value='10'>10</option><option value='20'>20</option><option value='50'>50</option>"));
         select.val(pageVo.pageSize);
         select.change(function () {
