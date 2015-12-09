853bd3ad6b8b30d9d5ce1b53696d10c35d048bd8
zhangjinpeng <A9cccbbo25>
2015-12-09 21:40:53 +0800

    log4J  依赖冲突

--- harem-web/src/test/java/dubbo/JUnitDubboTest.java
+++ harem-web/src/test/java/dubbo/JUnitDubboTest.java
@@ -2,15 +2,25 @@ package dubbo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.yimayhd.ic.client.service.item.ResourcePublishService;
+
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath*:spring-context-test.xml"})
+@ContextConfiguration(locations = {"classpath:spring-context-test.xml"})
 public class JUnitDubboTest {
 
+	@Autowired
+	ResourcePublishService resourcePublishService;
+	
 	@Test
 	public void test() {
+		
+		System.out.println();
+		System.out.println(resourcePublishService == null);
+		System.out.println();
 		
 	}
 	
