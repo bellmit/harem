a67a5fc68437dcae160523a4991fe0100f919456
zhangjinpeng <A9cccbbo25>
2015-12-09 17:42:53 +0800

    添加配置

--- harem-web/src/test/java/dubbo/JUnitDubboTest.java
+++ harem-web/src/test/java/dubbo/JUnitDubboTest.java
@@ -1,5 +1,17 @@
 package dubbo;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath*:spring-dubbo.xml"})
 public class JUnitDubboTest {
 
+	@Test
+	public void test() {
+		
+	}
+	
 }
