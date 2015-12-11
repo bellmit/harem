d76931b2c43d6a815c770fbc9fbcf62b7ef8126c
taoxiaoxian <tangqx@yimayholiday.com>
2015-12-11 11:22:55 +0800

    商贸bug

--- harem-web/src/main/webapp/WEB-INF/resources/js/common.js
+++ harem-web/src/main/webapp/WEB-INF/resources/js/common.js
@@ -22,3 +22,19 @@ function freshFrame(callback){
         }
     })
 }
+
+/************子页面操作刷新前一个子页面************/
+function freshPrevFrame(callback){
+    top.$(".tab-pane").each(function(){
+        if($(this).css("display") == "block"){
+            //console.log($(this));
+            var th = $(this)[0];
+            var parentTabId = $(th).attr("parenttabid");
+            var parentSrc = $("#"+parentTabId).find(".tabIframe").attr("src");
+            $("#"+parentTabId).find(".tabIframe").attr("src",parentSrc);
+            if(callback){
+                callback();
+            }
+        }
+    })
+}
