81d5127d161cd4d669122fc22441bff51024d760
zhangjinpeng <A9cccbbo25>
2015-12-11 15:59:51 +0800

    角色管理

--- harem-web/src/main/java/com/yimayhd/harem/controller/SystemManageController.java
+++ harem-web/src/main/java/com/yimayhd/harem/controller/SystemManageController.java
@@ -8,12 +8,15 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.alibaba.fastjson.JSON;
 import com.yimayhd.harem.base.BaseController;
 import com.yimayhd.harem.base.PageVO;
 import com.yimayhd.harem.model.Order;
 import com.yimayhd.harem.model.query.OrderListQuery;
+import com.yimayhd.harem.model.query.RoleListQuery;
 import com.yimayhd.harem.model.vo.OrderVO;
 import com.yimayhd.harem.service.OrderService;
+import com.yimayhd.harem.service.SystemManageService;
 
 @Controller
 @RequestMapping("/systemManage")
@@ -21,9 +24,12 @@ public class SystemManageController extends BaseController {
 
 	@Autowired
 	private OrderService orderService;
+	
+	@Autowired
+	SystemManageService systemManageService;
 
 	@RequestMapping(value = "/roleList", method = RequestMethod.GET)
-	public String membershipCardOrderList(Model model, OrderListQuery orderListQuery) throws Exception {
+	public String roleList(Model model, OrderListQuery orderListQuery) throws Exception {
 		OrderVO orderVO = new OrderVO();
 		orderVO.setOrderListQuery(orderListQuery);
 		List<Order> activityOrderList = orderService.getOrderList(orderVO);
@@ -34,16 +40,28 @@ public class SystemManageController extends BaseController {
 		return "/system/systemManage/roleList";
 	}
 	
-	@RequestMapping(value = "/roleList2", method = RequestMethod.GET)
-	public String membershipCardOrderList2(Model model, OrderListQuery orderListQuery) throws Exception {
-		OrderVO orderVO = new OrderVO();
+	@RequestMapping(value = "/roleList3", method = RequestMethod.GET)
+	public String roleList2() throws Exception {
+		
+		RoleListQuery roleListQuery = new RoleListQuery();
+		roleListQuery.setPageNumber(0);
+		roleListQuery.setPageSize(5);
+		roleListQuery.setRoleName("name");
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println(JSON.toJSONString(systemManageService.getListNew(roleListQuery)));
+		System.out.println();
+		System.out.println();
+		System.out.println();
+/*		OrderVO orderVO = new OrderVO();
 		orderVO.setOrderListQuery(orderListQuery);
 		List<Order> activityOrderList = orderService.getOrderList(orderVO);
 		PageVO<Order> pageVo = new PageVO<Order>(orderListQuery.getPageNumber(), orderListQuery.getPageSize(), 14800);
 		model.addAttribute("pageVo", pageVo);
 		model.addAttribute("orderListQuery", orderListQuery);
 		model.addAttribute("orderList", activityOrderList);
-		return "/system/systemManage/roleList";
+*/		return "/system/systemManage/roleList";
 	}
 	
 }
