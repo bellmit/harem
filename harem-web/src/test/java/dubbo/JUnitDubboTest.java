fff7bf6cbcf29446a7669b0e70dac35b3f02f3a9
zhangjinpeng <A9cccbbo25>
2015-12-15 20:20:49 +0800

    资源管理-酒店列表页

--- harem-web/src/test/java/dubbo/JUnitDubboTest.java
+++ harem-web/src/test/java/dubbo/JUnitDubboTest.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.alibaba.fastjson.JSON;
+import com.yimayhd.ic.client.model.query.HotelPageQuery;
 import com.yimayhd.ic.client.service.item.ResourcePublishService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -15,12 +17,25 @@ public class JUnitDubboTest {
 	@Autowired
 	ResourcePublishService resourcePublishService;
 	
+	@Autowired
+	com.yimayhd.ic.client.service.item.ItemQueryService itemQueryService;
+	
 	@Test
 	public void test() {
 		
 		System.out.println();
 		System.out.println(resourcePublishService == null);
 		System.out.println();
+		
+	}
+	
+	@Test
+	public void testItemQueryService() {
+		
+		com.yimayhd.ic.client.model.query.HotelPageQuery hotelPageQuery = new HotelPageQuery();
+		hotelPageQuery.setNeedCount(true);
+		hotelPageQuery.setPersonOrPhone("李大嘴");
+		System.out.println(JSON.toJSONString(itemQueryService.pageQueryHotel(hotelPageQuery)));
 		
 	}
 	
