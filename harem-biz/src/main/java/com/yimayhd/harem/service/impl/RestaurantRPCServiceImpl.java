cdde45c34d6ea25dba4481c518f9e8f287cb0152
zhangjinpeng <A9cccbbo25>
2015-12-17 18:42:38 +0800

    获取商品

--- harem-biz/src/main/java/com/yimayhd/harem/service/impl/RestaurantRPCServiceImpl.java
+++ harem-biz/src/main/java/com/yimayhd/harem/service/impl/RestaurantRPCServiceImpl.java
@@ -2,6 +2,7 @@ package com.yimayhd.harem.service.impl;
 
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.yimayhd.harem.base.PageVO;
@@ -26,6 +27,27 @@ public class RestaurantRPCServiceImpl implements RestaurantRPCService {
 		restaurantPageQuery.setNeedCount(true);
     	restaurantPageQuery.setPageNo(restaurantListQuery.getPageNumber());
     	restaurantPageQuery.setPageSize(restaurantListQuery.getPageSize());
+    	
+    	// 酒店名称
+    	if (StringUtils.isEmpty(restaurantListQuery.getName())) {   		
+    		restaurantPageQuery.setName(restaurantListQuery.getName());
+    	}
+    	// 状态
+    	if (restaurantListQuery.getStatus() != 0) {
+    		restaurantPageQuery.setStatus(restaurantListQuery.getStatus());
+    	}
+    	// 联系人
+    	if (StringUtils.isEmpty(restaurantListQuery.getPersonOrPhone())) {
+    		restaurantPageQuery.setPersonOrPhone(restaurantListQuery.getPersonOrPhone());
+    	}
+    	// 创建时间
+    	if (StringUtils.isEmpty(restaurantListQuery.getBeginTime())) {
+    		restaurantPageQuery.setStartTime(restaurantListQuery.getBeginTime());
+    	}
+    	// 结束时间
+    	if (StringUtils.isEmpty(restaurantListQuery.getEndTime())) {
+    		restaurantPageQuery.setEndTime(restaurantListQuery.getEndTime());
+    	}
     	
     	ICPageResult<RestaurantDO> icPageResult = itemQueryService.pageQueryRestaurant(restaurantPageQuery);
 		
