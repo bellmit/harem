9a01778a693efebee91a02f0294788c33a63d522
zhangjinpeng <A9cccbbo25>
2015-12-18 15:38:37 +0800

    添加注释  勿改userAddressServiceRef  bean的ID

---
+++ harem-biz/src/test/java/yimayhd/harem/service/impl/HaMenuServiceImplTest.java
@@ -0,0 +1,28 @@
+package yimayhd.harem.service.impl;
+
+import com.yimayhd.harem.service.HaMenuService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by Administrator on 2015/11/2.
+ */
+public class HaMenuServiceImplTest {
+
+    @RunWith(SpringJUnit4ClassRunner.class)
+    @ContextConfiguration(locations = {"classpath:*.xml"})
+    public class ServePublishServiceImplTest {
+
+        @Autowired
+        private HaMenuService haMenuService;
+
+        @Test
+        public void getMenuListByUserId(){
+
+        }
+
+    }
+}
