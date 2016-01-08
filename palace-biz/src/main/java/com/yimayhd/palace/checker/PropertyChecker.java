d6bad7c5e5a9fa17ed73854c76f775818c28c4eb
yebin <yebin@yimayholiday.com>
2016-01-08 20:52:43 +0800

    Property Checker

--- palace-biz/src/main/java/com/yimayhd/palace/checker/PropertyChecker.java
+++ palace-biz/src/main/java/com/yimayhd/palace/checker/PropertyChecker.java
@@ -1,6 +1,8 @@
 package com.yimayhd.palace.checker;
 
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.yimayhd.palace.checker.result.CheckResult;
 
@@ -11,27 +13,20 @@ import com.yimayhd.palace.checker.result.CheckResult;
  *
  */
 public class PropertyChecker {
+	private static final Logger log = LoggerFactory.getLogger(PropertyChecker.class);
+
 	public static CheckResult checkProperty(long pId, int pType, String pTxt) {
+		String temp = "Property验证失败: pId={},pType={},pTxt={}";
 		if (pId <= 0) {
+			log.error(temp, pId, pType, pTxt);
 			return CheckResult.error("无效PropertyID");
 		}
 		if (pType <= 0) {
+			log.error(temp, pId, pType, pTxt);
 			return CheckResult.error("无效Property类型");
 		}
 		if (StringUtils.isNotBlank(pTxt)) {
-			return CheckResult.error("Property名称不能为空");
-		}
-		return CheckResult.success();
-	}
-	
-	public static CheckResult checkPropertyValue(long pId, int pType, String pTxt) {
-		if (pId <= 0) {
-			return CheckResult.error("无效PropertyID");
-		}
-		if (pType <= 0) {
-			return CheckResult.error("无效Property类型");
-		}
-		if (StringUtils.isNotBlank(pTxt)) {
+			log.error(temp, pId, pType, pTxt);
 			return CheckResult.error("Property名称不能为空");
 		}
 		return CheckResult.success();
