6057ecd9454d4e0fd367ffff1387bbc0dc6b2887
zhaoyue <zhaoyue@yimayholiday.com>
2016-01-11 11:24:42 +0800

    增加佣金的controller

---
+++ palace-web/src/main/java/com/yimayhd/commission/controller/CommissionController.java
@@ -0,0 +1,16 @@
+package com.yimayhd.commission.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: zhaoyue
+ * Date: 2016/1/11
+ * Time: 11:23
+ * To change this template use File | Settings | File Templates.
+ */
+@Controller
+public class CommissionController  {
+
+    
+}
