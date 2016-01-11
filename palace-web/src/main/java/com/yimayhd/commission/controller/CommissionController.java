c92624fa7e507634d97af6cce990621e31cdcd1f
zhaoyue <zhaoyue@yimayholiday.com>
2016-01-11 14:36:09 +0800

    修改佣金配置

--- palace-web/src/main/java/com/yimayhd/commission/controller/CommissionController.java
+++ palace-web/src/main/java/com/yimayhd/commission/controller/CommissionController.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Controller;
  * To change this template use File | Settings | File Templates.
  */
 @Controller
+
 public class CommissionController  {
 
     private static final Logger logger = LoggerFactory.getLogger(CommissionController.class);
