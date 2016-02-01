c6b555d0c84acee8a63bfaf85e51b9db41cfe7c1
caozhangfeng <caozf@yimayholiday.com>
2016-02-01 11:13:04 +0800

    优惠管理

---
+++ palace-biz/src/main/java/com/yimayhd/gf/service/CommodityService.java
@@ -0,0 +1,72 @@
+package com.yimayhd.gf.service;
+
+import com.yimayhd.ic.client.model.domain.item.ItemDO;
+import com.yimayhd.palace.base.PageVO;
+import com.yimayhd.palace.model.ItemResultVO;
+import com.yimayhd.palace.model.ItemVO;
+import com.yimayhd.palace.model.query.CommodityListQuery;
+
+import java.util.List;
+
+/**
+ * Created by czf on 2015/11/24.
+ */
+public interface CommodityService {
+    /**
+     * 获取商品列表
+     * @param commodityListQuery
+     * @return
+     * @throws Exception
+     */
+    PageVO<ItemVO> getList(CommodityListQuery commodityListQuery)throws Exception;
+
+    /**
+     * 根据id获取商品信息
+     * @param id
+     * @return
+     * @throws Exception
+     */
+    ItemResultVO getCommodityById(long id)throws Exception;
+
+    /**
+     * 商品上架
+     * @param sellerId 商家id
+     * @param id 商品ID
+     */
+    void publish(long sellerId, long id)throws Exception;
+
+    /**
+     * 商品上架
+     * @param sellerId 商家id
+     * @param id 商品ID
+     */
+    void close(long sellerId, long id)throws Exception;
+
+    /**
+     *
+     * @param sellerId
+     * @param idList
+     */
+    void batchPublish(long sellerId, List<Long> idList);
+
+    /**
+     *
+     * @param sellerId
+     * @param idList
+     */
+    void batchClose(long sellerId, List<Long> idList);
+    /**
+     * 新增普通商品
+     * @param itemVO 普通商品表单对象
+     * @throws Exception
+     */
+    void addCommonItem(ItemVO itemVO)throws Exception;
+
+    /**
+     * 修改普通商品
+     * @param itemVO 普通商品表单对象
+     * @throws Exception
+     */
+    void modifyCommonItem(ItemVO itemVO)throws Exception;
+
+}
