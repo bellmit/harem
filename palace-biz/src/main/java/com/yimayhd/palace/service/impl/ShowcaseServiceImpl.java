064d43449a3deac0ff6856d2a96b2da772738053
caozhangfeng <caozf@yimayholiday.com>
2016-04-13 10:13:57 +0800

    no message

--- palace-biz/src/main/java/com/yimayhd/palace/service/impl/ShowcaseServiceImpl.java
+++ palace-biz/src/main/java/com/yimayhd/palace/service/impl/ShowcaseServiceImpl.java
@@ -1,9 +1,31 @@
 package com.yimayhd.palace.service.impl;
 
+import com.yimayhd.palace.model.vo.booth.ShowcaseVO;
 import com.yimayhd.palace.service.ShowcaseService;
+
+import java.util.List;
 
 /**
  * Created by Administrator on 2016/4/13.
  */
 public class ShowcaseServiceImpl implements ShowcaseService {
+    @Override
+    public List<ShowcaseVO> getList(long boothId) throws Exception {
+        return null;
+    }
+
+    @Override
+    public ShowcaseVO getById(long id) throws Exception {
+        return null;
+    }
+
+    @Override
+    public ShowcaseVO add(ShowcaseVO entity) throws Exception {
+        return null;
+    }
+
+    @Override
+    public ShowcaseVO modify(ShowcaseVO entity) throws Exception {
+        return null;
+    }
 }
