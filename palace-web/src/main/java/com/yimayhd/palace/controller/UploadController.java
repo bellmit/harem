799dcf783e88c830a50f6aeca3e129af582def9b
wuzhengfei <wuzf@yimayholiday.com>
2016-04-20 16:22:01 +0800

    bugfix

--- palace-web/src/main/java/com/yimayhd/palace/controller/UploadController.java
+++ palace-web/src/main/java/com/yimayhd/palace/controller/UploadController.java
@@ -6,13 +6,15 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.yimayhd.palace.constant.ResponseStatus;
 import org.apache.commons.lang.StringUtils;
 import org.apache.http.message.BasicNameValuePair;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,8 +25,12 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import com.taobao.common.tfs.TfsManager;
+import com.taobao.common.tfs.impl.TfsFile;
 import com.yimayhd.palace.base.BaseController;
 import com.yimayhd.palace.base.ResponseVo;
+import com.yimayhd.palace.constant.ResponseStatus;
+import com.yimayhd.palace.error.PalaceReturnCode;
+import com.yimayhd.palace.result.BizResult;
 import com.yimayhd.palace.util.WebResourceConfigUtil;
 import com.yimayhd.palace.util.excel.JxlFor2003;
 import com.yimayhd.palace.util.excel.TestPerson;
@@ -35,6 +41,7 @@ import com.yimayhd.palace.util.excel.TestPerson;
 @Controller
 @RequestMapping("/upload")
 public class UploadController extends BaseController {
+	private static final Logger logger = LoggerFactory.getLogger("UploadController");
 
 	@Autowired
 	private TfsManager tfsManager;
@@ -72,22 +79,31 @@ public class UploadController extends BaseController {
 	@ResponseBody
 	@RequestMapping(value = "/file", method = RequestMethod.POST)
 	public ResponseVo uploadFile(HttpServletRequest request) throws Exception {
-		MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
-		Iterator<String> iterator = multipartRequest.getFileNames();
-		MultipartFile multipartFile = multipartRequest.getFile(iterator.next());
-		String fileName = multipartFile.getOriginalFilename();
-		String suffix = "";
-		if (fileName.lastIndexOf(".") != -1) {
-			suffix = fileName.substring(fileName.lastIndexOf("."));
-		}
-		String tfsName = tfsManager.saveFile(multipartFile.getBytes(), null, suffix);
-		if(StringUtils.isNotBlank(tfsName) && !"null".equals(tfsName)){
-			tfsName += suffix;
-		}else{
-			return new ResponseVo(ResponseStatus.ERROR.VALUE,"图片上传失败");
-		}
+//		MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
+//		Iterator<String> iterator = multipartRequest.getFileNames();
+//		MultipartFile multipartFile = multipartRequest.getFile(iterator.next());
+//		String fileName = multipartFile.getOriginalFilename();
+//		String suffix = "";
+//		if (fileName.lastIndexOf(".") != -1) {
+//			suffix = fileName.substring(fileName.lastIndexOf("."));
+//		}
+//		String tfsName = tfsManager.saveFile(multipartFile.getBytes(), null, suffix);
+//		if(StringUtils.isNotBlank(tfsName) && !"null".equals(tfsName)){
+//			tfsName += suffix;
+//		}else{
+//			return new ResponseVo(ResponseStatus.ERROR.VALUE,"图片上传失败");
+//		}
+//		return new ResponseVo(tfsName);
 
-		return new ResponseVo(tfsName);
+		BizResult<String> result = upload(request);
+		if( result == null || !result.isSuccess() || StringUtils.isBlank(result.getValue())){
+			ResponseVo vo = new ResponseVo(ResponseStatus.ERROR);
+			return vo;
+		}else{
+			ResponseVo vo = new ResponseVo(result.getValue()) ;
+			return vo;
+		}
+		
 	}
 
 	/**
@@ -110,6 +126,10 @@ public class UploadController extends BaseController {
 			if (fileName.lastIndexOf(".") != -1) {
 				suffix = fileName.substring(fileName.lastIndexOf("."));
 			}
+			if (StringUtils.isBlank(suffix)) {
+				suffix = ".jpg";
+			}
+			
 			String tfsName = tfsManager.saveFile(multipartFile.getBytes(), null, suffix) + suffix;
 			if(StringUtils.isNotBlank(tfsName) && !"null".equals(tfsName)) {
 				// 除去后缀截取十五个字符
@@ -177,4 +197,69 @@ public class UploadController extends BaseController {
 		JxlFor2003.exportExcel(response, "测试.xls", testPersonList, headList);
 
 	}
+	
+
+    private static final String DOT = ".";
+    private static final int MAX_LENGTH = 4 * 1024 * 1024 ; 
+    
+	
+	private BizResult<String> upload(HttpServletRequest request) {
+		BizResult<String> result = new BizResult<String>();
+		try{
+			MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
+			
+			
+			Iterator<String> iterator = multipartRequest.getFileNames();
+			MultipartFile multipartFile = multipartRequest.getFile(iterator.next());
+			String fileName = multipartFile.getOriginalFilename();
+			long size = multipartFile.getSize();
+			if (size > MAX_LENGTH) {
+				result.setPalaceReturnCode(PalaceReturnCode.FILE_TO_BIG);
+				return result;
+			}
+			
+			String tfsName = upload(fileName, multipartFile.getBytes(), size);
+			if (tfsName == null) {// 上传失败
+				result.setPalaceReturnCode(PalaceReturnCode.UPLOAD_FILE_FAILED);
+				return result;
+			}
+			result.setValue(tfsName);
+			
+		}catch(Exception e){
+			logger.error("upload failed! ", e);
+			result.setPalaceReturnCode(PalaceReturnCode.UPLOAD_FILE_FAILED);
+		}
+		return result;
+	}
+	
+    private String upload(String fileName, byte[] bytes, long size){
+    	String log = "UUID="+UUID.randomUUID()+"  filename="+fileName+"  size="+size ;
+    	logger.info(log);
+		int index = fileName.lastIndexOf(DOT);
+		String suffix = index != -1 ? fileName.substring(index, fileName.length()) : null;
+		if (StringUtils.isBlank(suffix)) {
+			suffix = ".jpg";
+		}
+		String tfsName = null;
+		try {
+			if (size > MAX_LENGTH) {
+				return null;
+			} else if (size > TfsFile.MAX_SMALL_IO_LENGTH && size < MAX_LENGTH) {
+				String key = String.valueOf(System.currentTimeMillis());
+				tfsName = tfsManager.saveLargeFile(bytes, null, suffix, key);
+			} else {
+				tfsName = tfsManager.saveFile(bytes, null, suffix);
+			}
+
+		} catch (Exception e) {
+			logger.error("upload  filename={}  size=", fileName, size, e);
+		}
+		if (tfsName == null) {// 上传失败
+			return null;
+		}
+		String targetFileName = tfsName + suffix;
+		
+		logger.info(log+"  result="+targetFileName);
+		return targetFileName;
+    }
 }
