dce49921bebdc40dea16eb5c5e818ae47543a337
zhangxiaoyang <xiaoyangzhang1990@163.com>
2016-04-20 19:35:41 +0800

    11

--- palace-biz/src/main/java/com/yimayhd/palace/repo/MerchantRepo.java
+++ palace-biz/src/main/java/com/yimayhd/palace/repo/MerchantRepo.java
@@ -36,17 +36,22 @@ public class MerchantRepo {
 		BizResultSupport resultSupport = new BizResultSupport();
 		try {
 			BaseResult<MerchantDO> saveMerchantResult = userMerchantServiceRef.saveMerchant(vo.getMerchantDO(vo,sessionManager.getUserId()));
+			log.error("error is "+saveMerchantResult);
+			if (saveMerchantResult == null) {
+				resultSupport.init(false, -1, "保存失败");
+				return resultSupport;
+			}
 			if (!saveMerchantResult.isSuccess()) {
 				resultSupport.init(false, saveMerchantResult.getErrorCode(), saveMerchantResult.getErrorMsg());
 				//resultSupport.setPalaceReturnCode(PalaceReturnCode.ADD_MERCHANT_ERROR);
 			}
-			return resultSupport;
+			//return resultSupport;
 		} catch (Exception e) {
-			log.error("add merchant of food error and params:"+JSON.toJSONString(vo)+"and exception is "+e.getMessage());
+			log.error("add merchant of food error and params:"+JSON.toJSONString(vo)+"and exception is "+e);
 			resultSupport.setPalaceReturnCode(PalaceReturnCode.ADD_MERCHANT_ERROR);
 			
-			return resultSupport;
 		}
+		return resultSupport;
 		
 		
 	}
@@ -59,18 +64,23 @@ public class MerchantRepo {
 			//MerchantDTO dto = new MerchantDTO();
 			//dto.sets
 			BaseResult<Boolean> updateMerchantResult = userMerchantServiceRef.updateMerchantInfo(vo.getMerchantDTO(vo));
+			log.error("error is "+updateMerchantResult);
+			if (updateMerchantResult == null) {
+				resultSupport.init(false, -1, "保存失败");
+				return resultSupport;
+			}
 			if (!updateMerchantResult.isSuccess()) {
 				
 				//resultSupport.setPalaceReturnCode(PalaceReturnCode.UPDATE_MERCHANT_ERROR);
 				resultSupport.init(false, updateMerchantResult.getErrorCode(), updateMerchantResult.getErrorMsg());
 			}
-			return resultSupport;
+			//return resultSupport;
 		} catch (Exception e) {
 			log.error("update merchant of food error and params:"+JSON.toJSONString(vo)+"and exception is "+e.getMessage());
 			resultSupport.setPalaceReturnCode(PalaceReturnCode.UPDATE_MERCHANT_ERROR);
 			
-			return resultSupport;
 		}
+		return resultSupport;
 		
 	}
 	public BizResultSupport batchUpdateMerchant(List<Long> idList,int status) {
