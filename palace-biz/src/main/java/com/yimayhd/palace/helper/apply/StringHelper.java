f0e129f28b4810102c6338239f72158c9883cca7
zhangxiaoyang <xiaoyangzhang1990@163.com>
2016-04-21 15:46:05 +0800

    人均价格显示问题

--- palace-biz/src/main/java/com/yimayhd/palace/helper/apply/StringHelper.java
+++ palace-biz/src/main/java/com/yimayhd/palace/helper/apply/StringHelper.java
@@ -8,6 +8,7 @@ public class StringHelper {
 	public static String replaceBlank(String str) {
         String dest = "";
         if (str!=null) {
+        	str = str.trim();
             Pattern p = Pattern.compile("[\t|\r|\n]w+[\t|\r|\n]");
             Matcher m = p.matcher(str);
             dest = m.replaceAll("");
