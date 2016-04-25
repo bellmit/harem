bd7ef0a0b682d08695134ab7113d00a00b8aeade
zhangxiaoyang <xiaoyangzhang1990@163.com>
2016-04-25 20:23:40 +0800

    格式化问题

--- palace-biz/src/main/java/com/yimayhd/palace/helper/apply/StringHelper.java
+++ palace-biz/src/main/java/com/yimayhd/palace/helper/apply/StringHelper.java
@@ -8,10 +8,10 @@ public class StringHelper {
 	public static String replaceBlank(String str) {
         String dest = "";
         if (str!=null) {
-        	str = str.trim();
-            Pattern p = Pattern.compile("[\t|\r|\n]w+[\t|\r|\n]");
-            Matcher m = p.matcher(str);
-            dest = m.replaceAll("");
+//        	str = str.trim();
+//            Pattern p = Pattern.compile("[\t|\r|\n]w+[\t|\r|\n]");
+//            Matcher m = p.matcher(str);
+            dest = str.replaceAll("^[　*| *| *|//s*]*", "").replaceAll("[　*| *| *|//s*]*$", "");
         }
         return dest;
     }
