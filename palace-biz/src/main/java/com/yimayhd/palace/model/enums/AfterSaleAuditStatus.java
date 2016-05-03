8339ad6ca3d328a60969f1c826221304ef1c0095
yusw <yusw>
2016-05-03 19:10:22 +0800

    vm修改

---
+++ palace-biz/src/main/java/com/yimayhd/palace/model/enums/AfterSaleAuditStatus.java
@@ -0,0 +1,10 @@
+package com.yimayhd.palace.model.enums;
+
+/**
+ * @param $params
+ * @author create by yushengwei on 2016/5/3
+ * @Description
+ * @return $returns
+ */
+public enum AfterSaleAuditStatus {
+}
