cb2c6309b4e048255e7ece19591d3c464545d2b2
xinzhanguo <xinzg@yimayholiday.com>
2016-06-22 14:22:24 +0800

    Added fcode 后台 f码活动创建

--- palace-biz/src/main/java/com/yimayhd/palace/model/query/VoucherListQuery.java
+++ palace-biz/src/main/java/com/yimayhd/palace/model/query/VoucherListQuery.java
@@ -2,17 +2,38 @@ package com.yimayhd.palace.model.query;
 
 import com.yimayhd.palace.base.BaseQuery;
 
+import java.util.List;
+
 /**
  * Created by czf on 2016/1/11.
  */
 public class VoucherListQuery extends BaseQuery{
 
+    private String title;//标题
+    private int entityType;
+    private int voucherType;//券类型
+    private int status;
+    private String issueType;//发卷方式
     private String beginDate;
     private String endDate;
-    private int status;
-    private int voucherType;//券类型
-    private String title;//标题
-    private String issueType;//发卷方式
+    private String userName;
+    private String vid;
+
+    public String getVid() {
+        return vid;
+    }
+
+    public void setVid(String vid) {
+        this.vid = vid;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
 
     public String getBeginDate() {
         return beginDate;
@@ -30,12 +51,23 @@ public class VoucherListQuery extends BaseQuery{
         this.endDate = endDate;
     }
 
-    public int getStatus() {
-        return status;
+    private List<Integer> ids;
+    private long userId;
+
+    public String getTitle() {
+        return title;
     }
 
-    public void setStatus(int status) {
-        this.status = status;
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getEntityType() {
+        return entityType;
+    }
+
+    public void setEntityType(int entityType) {
+        this.entityType = entityType;
     }
 
     public int getVoucherType() {
@@ -46,23 +78,37 @@ public class VoucherListQuery extends BaseQuery{
         this.voucherType = voucherType;
     }
 
-    public String getTitle() {
-        return title;
+    public int getStatus() {
+        return status;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
+    public void setStatus(int status) {
+        this.status = status;
     }
 
-	public String getIssueType() {
-		return issueType;
-	}
+    public String getIssueType() {
+        return issueType;
+    }
 
-	public void setIssueType(String issueType) {
-		this.issueType = issueType;
-	}
+    public void setIssueType(String issueType) {
+        this.issueType = issueType;
+    }
 
 
-    
-    
+    public List<Integer> getIds() {
+        return ids;
+    }
+
+    public void setIds(List<Integer> ids) {
+        this.ids = ids;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
 }
---
+++ palace-web/src/main/java/com/yimayhd/palace/controller/FCodeController.java
@@ -0,0 +1,189 @@
+package com.yimayhd.palace.controller;
+
+import com.yimayhd.ic.client.service.item.ItemPublishService;
+import com.yimayhd.voucher.client.result.VcBaseResult;
+import com.yimayhd.palace.base.BaseController;
+import com.yimayhd.palace.base.PageVO;
+import com.yimayhd.palace.model.query.VoucherListQuery;
+import com.yimayhd.palace.model.vo.VoucherTemplateVO;
+import com.yimayhd.palace.service.VoucherTemplateService;
+import com.yimayhd.user.client.domain.UserDO;
+import com.yimayhd.user.session.manager.SessionManager;
+import com.yimayhd.voucher.client.domain.VoucherDO;
+import com.yimayhd.voucher.client.enums.IssueType;
+import com.yimayhd.voucher.client.enums.EntityType;
+import com.yimayhd.voucher.client.enums.VoucherTemplateStatus;
+import com.yimayhd.voucher.client.enums.VoucherType;
+import com.yimayhd.voucher.client.query.VoucherPageQuery;
+import com.yimayhd.voucher.client.result.VcBasePageResult;
+import com.yimayhd.ic.client.model.result.item.ItemPubResult;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.BufferedOutputStream;
+import java.io.OutputStream;
+import java.text.ParseException;
+import java.util.*;
+
+
+/**
+ * Created by xinzhanguo on 6/16/16.
+ */
+@Controller
+@RequestMapping("/GF/fcodeManage")
+public class FCodeController extends BaseController {
+
+    @Autowired
+    private VoucherTemplateService voucherTemplateService;
+
+    @Autowired
+    private ItemPublishService itemPublishService;
+
+    @Autowired
+    private SessionManager sessionManager;
+
+    /**
+     * F码活动列表
+     *
+     * @return F码活动列表
+     * @throws Exception
+     */
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    public String list(Model model, VoucherListQuery voucherListQuery) throws Exception {
+
+        voucherListQuery.setVoucherType(VoucherType.RAND_CODE.getType());
+        PageVO<VoucherTemplateVO> pageVO = voucherTemplateService
+                .getList(voucherListQuery);
+
+        model.addAttribute("fCodeListQuery", voucherListQuery);
+        model.addAttribute("fCodeTemplateList", pageVO.getItemList());
+        model.addAttribute("pageVo", pageVO);
+        return "/system/fcode/list";
+    }
+
+    /**
+     * 新增F码活动
+     *
+     * @return 新增F码活动
+     * @throws Exception
+     */
+    @RequestMapping(value = "/toAdd", method = RequestMethod.GET)
+    public String toAdd() throws Exception {
+        return "/system/fcode/edit";
+    }
+    /**
+     * 新增F码活动
+     *
+     * @return 新增F码活动
+     * @throws Exception
+     */
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public String add(VoucherTemplateVO voucherTemplateVO) throws Exception {
+        UserDO userDO = sessionManager.getUser();
+        if (userDO != null) {
+            voucherTemplateVO.setOperator(userDO.getNickname());
+            // 关联创建人iD
+            voucherTemplateVO.setUserId(userDO.getId());
+        }
+
+        // 随机F码
+        voucherTemplateVO.setVoucherType(VoucherType.RAND_CODE.getType());
+        //关联商品
+        voucherTemplateVO.setEntityType(EntityType.ITEM.getType());
+
+        long itemId = voucherTemplateVO.getEntityId();
+        if(itemId != 0) {
+            ItemPubResult result = itemPublishService.changeToFItem(itemId);
+            if(!result.isSuccess()){
+                return "/error";
+            }
+        } else {
+            return "/error";
+        }
+
+        // 生成方式 预先生成
+        voucherTemplateVO.setIssueType(IssueType.PRE_GENERATE_WITH_CODE.getType());
+
+        voucherTemplateVO.setDomain(1100);
+
+        // 新增默认下架状态
+        voucherTemplateVO.setRequirement(Math.round(voucherTemplateVO
+                .getRequirement_() * 100));
+        voucherTemplateVO.setValue(Math.round(voucherTemplateVO.getValue_() * 100));
+
+        voucherTemplateVO.setStartTime(new Date());
+        voucherTemplateVO.setEndTime(getEndTime());
+
+        voucherTemplateVO.setStatus(VoucherTemplateStatus.INIT.getStatus());
+        VcBaseResult<Long> vcBaseResult = voucherTemplateService.add(voucherTemplateVO);
+        if(!vcBaseResult.isSuccess()) {
+            return "/error";
+        }
+
+        return "/success";
+    }
+    private Date getEndTime() throws ParseException {
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.YEAR, 1);
+        Date oneyearafter = calendar.getTime();
+        return oneyearafter;
+    }
+    /**
+     * 导出F码
+     *
+     * @param model
+     * @return
+     * @throws Exception
+     */
+    @RequestMapping(value = "/export/{id}")
+    @ResponseBody
+    public String export(Model model, @PathVariable(value = "id") long id)
+            throws Exception {
+        try {
+            String fileName = "export.csv";
+            StringBuilder sb = new StringBuilder();
+
+            VoucherPageQuery voucherPageQuery = new VoucherPageQuery();
+            voucherPageQuery.setVoucherTemplateId(id);
+            Integer pageNo = 1;
+            voucherPageQuery.setPageSize(100);
+            voucherPageQuery.setHasNextMod(true);
+            VcBasePageResult<VoucherDO> result = null;
+            do {
+                voucherPageQuery.setPageNo(pageNo);
+                result = voucherTemplateService.exportAllVouchers(voucherPageQuery);
+                if (result.isSuccess()) {
+                    for (int i = 0; i < result.getList().size(); i++) {
+                        sb.append(result.getList().get(i).getVoucherCode() + "\r\n");
+                    }
+                }
+                pageNo++;
+            } while (result.isSuccess() && result.isHasNext());
+
+            String str = sb.toString();
+
+            // 以流的形式下载文件。
+            byte[] buffer = str.getBytes();
+            // 清空response
+            response.reset();
+            // 设置response的Header
+            response.addHeader("Content-Disposition", "attachment;filename="
+                    + new String(fileName.getBytes()));
+            // response.addHeader("Content-Length", "" + file.length());
+            OutputStream toClient = new BufferedOutputStream(
+                    response.getOutputStream());
+            response.setContentType("application/octet-stream");
+            toClient.write(buffer);
+            toClient.flush();
+            toClient.close();
+        } catch (Exception e) {
+            log.error(e.getMessage(),e);
+            e.printStackTrace();
+        }
+
+        return "";
+    }
+}
