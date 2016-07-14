270c785ef19c7f709a968cb4f2dda0bb47f41af4
xiemn <xiemn@jiuxiulvxing.com>
2016-07-14 20:04:27 +0800

    达人故事H5

---
+++ palace-biz/src/main/java/com/yimayhd/palace/model/ArticleProductItemVO.java
@@ -0,0 +1,20 @@
+package com.yimayhd.palace.model;
+
+import java.io.Serializable;
+
+/**
+ * H5文章
+ * 
+ * @author xiemingna
+ *
+ */
+public class ArticleProductItemVO implements Serializable {
+	private static final long serialVersionUID = 1L;
+	public float itemPrice;
+	public String itemTitle;
+	public String merchantName;
+	public String merchantLogo;
+	public String itemType;
+	public String itemPic;
+	public String itemPicTitle;
+}
