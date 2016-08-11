792aa9fd930ea603edf4ec05ab86d4be486c7c42
guohongfei <guohf@yimayholiday.com>
2016-08-11 15:25:50 +0800

    bugfix

--- palace-biz/src/main/java/com/yimayhd/palace/service/impl/AccountServiceImpl.java
+++ palace-biz/src/main/java/com/yimayhd/palace/service/impl/AccountServiceImpl.java
@@ -3,7 +3,6 @@ package com.yimayhd.palace.service.impl;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
