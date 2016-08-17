c2bd511d92d2131e7ad6e8cece1679d013aaaf0b
liuxp <liuxp@jiuxiulvxing.com>
2016-08-17 17:42:26 +0800

    guide

---
+++ palace-biz/src/main/java/com/yimayhd/palace/repo/GuideRepo.java
@@ -0,0 +1,12 @@
+package com.yimayhd.palace.repo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * Created by liuxp on 2016/8/17.
+ */
+public class GuideRepo {
+
+    @Autowired
+    private GuideRepo guideServiceRef;
+}
