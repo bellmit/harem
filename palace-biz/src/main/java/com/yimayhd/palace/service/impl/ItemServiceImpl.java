136dfbb2128d1f1a5d8ecc407fa39ec5c18b098f
wangdi1 <wangdi1@jiuxiulvxing.com>
2016-08-18 21:24:57 +0800

    商户小v 店铺名称获取

--- palace-biz/src/main/java/com/yimayhd/palace/service/impl/ItemServiceImpl.java
+++ palace-biz/src/main/java/com/yimayhd/palace/service/impl/ItemServiceImpl.java
@@ -225,7 +225,7 @@ public class ItemServiceImpl implements ItemService {
 			// 只有昵称
 			icMerchantVO.setUserNick(sellerUser.getNickname());
 		}
-		if(UserOptions.COMMERCIAL_TENANT.has(sellerUser.getOptions())){
+		if(UserOptions.COMMERCIAL_TENANT.has(sellerUser.getOptions())||UserOptions.CERTIFICATED.has(sellerUser.getOptions())){
 			/**商户**/
 			//  店铺信息, 昵称用user
 			BaseResult<MerchantUserDTO> merchantUserResult = userMerchantServiceRef.getMerchantAndUserBySellerId(sellerId, Constant.DOMAIN_JIUXIU);
