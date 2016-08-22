ad1f86c3f2f24eda25a2b04e435f506eef7412ad
xushubing <xusb@jiuxiulvxing.com>
2016-08-22 14:22:52 +0800

    0920 导览

--- palace-web/src/main/java/com/yimayhd/palace/controller/AttachmentManageController.java
+++ palace-web/src/main/java/com/yimayhd/palace/controller/AttachmentManageController.java
@@ -1,7 +1,16 @@
 package com.yimayhd.palace.controller;
 
+import com.yimayhd.palace.base.PageVO;
+import com.yimayhd.palace.model.attachment.AttachmentVO;
+import com.yimayhd.palace.model.guide.GuideScenicVO;
+import com.yimayhd.palace.service.AttachmentManageService;
+import com.yimayhd.resourcecenter.model.query.MediaPageQuery;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.annotation.Resource;
 
 /**
  * 附件中心 音频管理 导览
@@ -10,4 +19,107 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/jiuniu/attachmentManage")
 public class AttachmentManageController {
+    @Resource
+    private AttachmentManageService attachmentManageService;
+
+    /**
+     * 分页
+     *
+     * @param model
+     * @param mediaPageQuery
+     * @return
+     * @throws Exception
+     */
+    @RequestMapping(value = "/list")
+    public String list(Model model, MediaPageQuery mediaPageQuery) throws Exception {
+        PageVO<AttachmentVO> pageVO = attachmentManageService.getAttachmentList(mediaPageQuery);
+
+        model.addAttribute("pageVo", pageVO);
+        model.addAttribute("itemList", pageVO.getItemList());
+        return "/system/attachment/list";
+    }
+
+    /**
+     * 添加导览跳转
+     *
+     * @return
+     * @throws Exception
+     */
+    @RequestMapping(value = "/toAdd")
+    public String toAdd() throws Exception {
+        return "/system/attachment/addAttachment";
+    }
+
+    /**
+     * 编辑导览跳转
+     *
+     * @return
+     * @throws Exception
+     */
+    @RequestMapping(value = "/toEdit")
+    public String toEdit() throws Exception {
+        return null;
+    }
+
+    /**
+     * 添加导览
+     *
+     * @return
+     * @throws Exception
+     */
+    @RequestMapping(value = "/addGuide", method = RequestMethod.POST)
+    public String addGuide(Model model, GuideScenicVO guideVO) throws Exception {
+        return null;
+    }
+
+    /**
+     * 修改导览
+     *
+     * @return
+     * @throws Exception
+     */
+    @RequestMapping(value = "/editGuide", method = RequestMethod.POST)
+    public String editGuide(Model model, GuideScenicVO guideVO) throws Exception {
+        return null;
+    }
+
+    /**
+     * 设置权重
+     *
+     * @param model
+     * @param guideVO
+     * @return
+     * @throws Exception
+     */
+
+    @RequestMapping(value = "/setWeight", method = RequestMethod.POST)
+    public String setWeight(Model model, GuideScenicVO guideVO) throws Exception {
+        return null;
+    }
+
+    /**
+     * 上架
+     *
+     * @param model
+     * @param guideId
+     * @return
+     * @throws Exception
+     */
+    @RequestMapping(value = "/status/up")
+    public String upStatus(Model model, long guideId) throws Exception {
+        return null;
+    }
+
+    /**
+     * 下架
+     *
+     * @param model
+     * @param guideId
+     * @return
+     * @throws Exception
+     */
+    @RequestMapping(value = "/status/down")
+    public String downStatus(Model model, long guideId) throws Exception {
+        return null;
+    }
 }
