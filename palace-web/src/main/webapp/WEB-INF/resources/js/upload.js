1f0413558ce67be32cb79718049d8d3995f7e02d
jinjialin <jinjl@jiuxiulvxing.com>
2016-09-12 15:07:53 +0800

    修改图片大小上传校验

--- palace-web/src/main/webapp/WEB-INF/resources/js/upload.js
+++ palace-web/src/main/webapp/WEB-INF/resources/js/upload.js
@@ -1,28 +1,28 @@
 //不是插件
-var fileUpload = function(id,type,callBack,fileSize){//id：上传控件筛选器（‘#id’或‘。class’），type：1单文件；2多文件，callBack：回调函数接收data，fileSize：限制上传大小
+var fileUpload = function(id, type, callBack, fileSize) { //id：上传控件筛选器（‘#id’或‘。class’），type：1单文件；2多文件，callBack：回调函数接收data，fileSize：限制上传大小
     var oFiles = document.querySelector(id).files;
-    uploadSenedFile(oFiles,type,callBack,fileSize);
+    uploadSenedFile(oFiles, type, callBack, fileSize);
 }
 
-var uploadSenedFile = function(files,type,callBack,fileSize){//id：上传控件筛选器（‘#id’或‘。class’），type：1单文件；2多文件，callBack：回调函数接收data，fileSize：限制上传大小
+var uploadSenedFile = function(files, type, callBack, fileSize) { //id：上传控件筛选器（‘#id’或‘。class’），type：1单文件；2多文件，callBack：回调函数接收data，fileSize：限制上传大小
     var oFiles = files;
     // 实例化一个表单数据对象
     var formData = new FormData();
     // 遍历文件列表，插入到表单数据中
     for (var i = 0, file; file = oFiles[i]; i++) {
-    	if(fileSize && file.size > fileSize * 1024){
-    		layer.msg('图片不能大于'+ fileSize +'K',{icon:2});
+        if (fileSize && file.size > fileSize * 1024) {
+            layer.msg('图片不能大于' + formatSize(fileSize*1024), { icon: 2 });
             return;
-    	}else if(file.size > 2000 * 1024){
-            layer.msg('图片不能大于2M',{icon:2});
+        } else if (file.size > 4 * 1024 * 1024) {
+            layer.msg('图片不能大于4M', { icon: 2 });
             return;
         }
-    	
-    	var fileName = file.name; 
-    	var fileType = (fileName.substring(fileName.lastIndexOf(".")+1,fileName.length)).toLowerCase();
-        if(fileType != "jpg" && fileType != "jpeg" && fileType != "png"){
-        	layer.msg('图片格式为JPG,JPEG,PNG',{icon:2});
-        	return;
+
+        var fileName = file.name;
+        var fileType = (fileName.substring(fileName.lastIndexOf(".") + 1, fileName.length)).toLowerCase();
+        if (fileType != "jpg" && fileType != "jpeg" && fileType != "png") {
+            layer.msg('图片格式为JPG,JPEG,PNG', { icon: 2 });
+            return;
         }
         // 文件名称，文件对象
         formData.append(fileName, file);
@@ -30,33 +30,46 @@ var uploadSenedFile = function(files,type,callBack,fileSize){//id：上传控件
     var xhr = new XMLHttpRequest();
     xhr.onload = function(data) {
         var data = JSON.parse(data.target.response);
-        if(data && data.status == 200){
+        if (data && data.status == 200) {
             var errMessage = new Array();
-            for(key in data.data){
-                if(!data.data[key] || data.data[key] === "null" ){
+            for (key in data.data) {
+                if (!data.data[key] || data.data[key] === "null") {
                     delete data.data[key];
                     errMessage.push(key);
                 }
             }
-            if(errMessage.length > 0){
-                layer.msg("以下图片上传失败：" + errMessage.join(','),{icon:2});
+            if (errMessage.length > 0) {
+                layer.msg("以下图片上传失败：" + errMessage.join(','), { icon: 2 });
             }
         }
         callBack(data);
 
     };
     //xhr.addEventListener("load", uploadComplete, false);
-    xhr.addEventListener("error", function(a,b,data){
-        layer.msg('提交出错',{icon:2});
+    xhr.addEventListener("error", function(a, b, data) {
+        layer.msg('提交出错', { icon: 2 });
     }, false);
-    xhr.addEventListener("abort", function(a,b,data){
-        layer.msg('提交出错',{icon:2});
+    xhr.addEventListener("abort", function(a, b, data) {
+        layer.msg('提交出错', { icon: 2 });
     }, false);
-    if(type==1){
+    if (type == 1) {
         xhr.open("POST", uploadFile, true);
-    }else{
-        xhr.open("POST",  uploadFiles, true);
+    } else {
+        xhr.open("POST", uploadFiles, true);
     }
     // 发送表单数据
     xhr.send(formData);
+}
+
+//格式化文件大小，输出成带单位的字符串
+function formatSize(size,units) {
+    var unit;
+
+    units = units || ['B', 'K', 'M', 'G', 'TB'];
+
+    while ((unit = units.shift()) && size > 1024) {
+        size = size / 1024;
+    }
+
+    return (unit === 'B' ? size : size.toFixed(0)) + unit;
 }
