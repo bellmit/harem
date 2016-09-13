1613d13745e0660a6570251b18b81b54cd4a648c
zhangxiaoyang <xiaoyangzhang1990@163.com>
2016-09-13 15:15:51 +0800

    添加订单详情页联系人邮箱

--- palace-biz/src/main/java/com/yimayhd/palace/model/ItemListQuery.java
+++ palace-biz/src/main/java/com/yimayhd/palace/model/ItemListQuery.java
@@ -18,11 +18,39 @@ public class ItemListQuery extends BaseQuery {
 	private Integer status;// 状态
 	private Date beginDate;// 发布开始时间
 	private Date endDate;// 发布结束时间
-	private String merchantName; //商户名称（店铺名称或昵称）
+	private String merchantName; //商户名称
 	private Long outId;//外部id
 	private Integer outType;//外部类型 参考 ResourceType
 	
 	private String orderNumFilter;//商品权重
+	private String sellerName;//店铺名称或者达人昵称
+	private String nickName;
+	private String shopName;
+	
+	public String getShopName() {
+		return shopName;
+	}
+
+	public void setShopName(String shopName) {
+		this.shopName = shopName;
+	}
+
+	public String getNickName() {
+		return nickName;
+	}
+
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+
+	public String getSellerName() {
+		return sellerName;
+	}
+
+	public void setSellerName(String sellerName) {
+		this.sellerName = sellerName;
+	}
+
 	private long sellerId;
 	
 	public long getSellerId() {
